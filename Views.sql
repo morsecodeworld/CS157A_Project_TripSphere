cf2dba75479315abaaebe73cbf522d70cd9ff259
morsecodeworld <91174612+morsecodeworld@users.noreply.github.com>
2023-12-01 11:43:13 -0800

    Add files via upload

---
+++ Views.sql
@@ -0,0 +1,36 @@
+
+SELECT 
+TRIP.Trip_ID,
+TRIP.Trip_Name, 
+TRIP.Start_Date, 
+TRIP.End_Date, 
+TRIP.Destination 
+FROM TRIP;
+
+
+SELECT  
+    TRIP.Trip_Name,
+    TRIP.Destination,   
+    TRANSPORTATION.Transportation_Type,
+    TRANSPORTATION.Departure_Location,
+    TRANSPORTATION.Arrival_Location,
+    TRANSPORTATION.Departure_Date,
+    TRANSPORTATION.Return_Date
+FROM TRANSPORTATION
+INNER JOIN TRIP ON TRIP.Trip_ID = TRANSPORTATION.Trip_ID;
+
+
+SELECT
+    TRIP.Trip_Name,
+    TRIP.Destination,
+    ACCOMMODATION.Accommodation_Name,
+    ACCOMMODATION.Accommodation_Type,
+    ACCOMMODATION.City,
+    ACCOMMODATION.State,
+    ACCOMMODATION.Phone_Number
+FROM ACCOMMODATION
+INNER JOIN TRIP ON TRIP.Trip_ID = ACCOMMODATION.Trip_ID;
+
+
+
+
